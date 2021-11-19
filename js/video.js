e77db9c6ad11349275baca2055909db00116d933
juanmthompson <78036285+juanmthompson@users.noreply.github.com>
2021-11-18 20:04:31 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -10,6 +10,7 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play();
+	vidvol.innerHTML = video.volume * 100 + "%";
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
